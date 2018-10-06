9df8e216a7561851d02e60f15a0e29d5674913b7
Thomas Tsiakalakis <tt@dplusc.de>
2018-10-06 17:14:33 +0200

    basic functionality

--- index.php
+++ index.php
@@ -3,8 +3,15 @@ $apibase = 'http://hermes.net.wurstsalat.cloud';
 
 require __DIR__ . '/vendor/autoload.php';
 
+if(array_key_exists('action', $_GET) && $_GET['action'] == 'set'){
+    file_get_contents($apibase . '/set?' . urlencode($_GET['id']) . '=' . urlencode($_GET['value']));
+    header('Location: .');
+    die();
+}
+
+
 $starttime = microtime(true);
-$items = json_decode(file_get_contents($apibase . '/list'));
+$items = json_decode(file_get_contents($apibase . '/list?values=1'));
 $endtime = microtime(true);
 
 $duration = $endtime - $starttime;
@@ -12,7 +19,7 @@ header('X-Duration: ' . number_format($duration * 1000, 2, '.', '') . 'ms');
 
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
 $twig = new Twig_Environment($loader, [
-    'debug' => false,
+    'debug' => true,
     'cache' => __DIR__ . '/cache',
 ]);
 
