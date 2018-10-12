211ea1900803321a86edad247a4b1a7b1cb08407
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2018-10-13 01:35:47 +0200

    animate menu scrolling

--- components/menu/index.js
+++ components/menu/index.js
@@ -5,23 +5,47 @@ import {
   render
 } from '../../dom.js';
 
-const scrollOffset = 10;
+const animationDuration = 150;
+
+function easing(t) {
+  return t < 0.5 ? 2 * t * t : -1 + ((4 - (2 * t)) * t);
+}
 
 export class Menu extends BaseComponent {
-  // _animateScroll(target) {
+  _animateScroll(to) {
+    const start = Date.now();
+    const from = this.scrollLeft;
+    const distance = to - from;
 
-  // }
+    if (!distance) return;
+
+    const tick = () => {
+      const progress = Math.min(1, Math.max(0, (Date.now() - start) / animationDuration));
+      const step = distance * easing(progress);
+
+      this.scrollLeft = Math.max(0, from + step);
+
+      if (progress >= 1) return;
+      window.requestAnimationFrame(tick);
+    };
+
+    tick();
+  }
 
   _handleChange(index) {
+    if (!this.mediaQuery || this.mediaQuery.matches) return;
+
     window.setTimeout(() => {
       const { offsetLeft, scrollWidth } = this.childNodes[index];
 
-      const scrollLeft = Math.max(0, offsetLeft
-        + scrollOffset
-        + (scrollWidth / 2)
-        - (this.offsetWidth / 2));
+      const scrollLeft = Math.min(
+        this.scrollWidth - this.offsetWidth,
+        Math.max(0, offsetLeft
+          + (scrollWidth / 2)
+          - (this.offsetWidth / 2))
+      );
 
-      this.scrollLeft = scrollLeft;
+      this._animateScroll(scrollLeft);
     }, 0);
   }
 
@@ -42,6 +66,8 @@ export class Menu extends BaseComponent {
       '_selectedRoom',
       this._handleChange.bind(this)
     );
+
+    this.mediaQuery = window.matchMedia('screen and (min-width: 769px)');
 
     this._handleChange(
       window.componentState.get('_selectedRoom')
