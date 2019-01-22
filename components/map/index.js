580fe246878f7f2e426913d77a08858f93487cf3
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2019-01-22 21:00:05 +0100

    map zoom

--- components/map/index.js
+++ components/map/index.js
@@ -4,6 +4,12 @@ import {
   bem
 } from '../../dom.js';
 
+import { MobileZoom } from './mobile-zoom.js';
+
+const viewBox = {
+  x: 1578,
+  y: 1483
+};
 const activeClass = bem('extension', null, 'active');
 
 export class MapComponent extends BaseComponent {
@@ -12,12 +18,51 @@ export class MapComponent extends BaseComponent {
 
     if (id === value) {
       this.classList.add(activeClass);
+      if (this.mz) {
+        this.mz.reset();
+      }
     } else {
       this.classList.remove(activeClass);
     }
   }
 
   create() {
+    const svgGroup = this.shadowRoot.querySelector('.all');
+
+    this.mz = new MobileZoom(
+      this,
+      (translation, distance) => {
+        const { width, height } = svgGroup.getBoundingClientRect();
+
+        const size = Math.min(width, height);
+        const scale = Math.min(((distance * 2 / size) + 1) ** 2, 3);
+
+        const translate = {
+          x: Math.max(
+            Math.min(
+              (translation.x * 2 / width) * viewBox.x, viewBox.x / 2
+            ), -viewBox.x / 2
+          ),
+          y: Math.max(
+            Math.min(
+              (translation.y * 2 / height) * viewBox.y, viewBox.y / 2
+            ), -viewBox.y / 2
+          )
+        };
+
+        const origin = {
+          x: Math.max(Math.min((-translation.x * 2) + (width / 2), width), 0),
+          y: Math.max(Math.min((-translation.y * 2) + (height / 2), height), 0)
+        };
+
+        svgGroup.style.transformOrigin = `${origin.x}px ${origin.y}px`;
+        svgGroup.style.transform = [
+          `scale(${scale})`,
+          `translate(${translate.x}px, ${translate.y}px)`
+        ].join(' ');
+      }
+    );
+
     window.xState.subscribe('*', (value, key) => {
       const element = this.shadowRoot.getElementById(key);
       if (!element) return;
