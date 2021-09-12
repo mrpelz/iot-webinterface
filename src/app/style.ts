2be4f62f73d21e6aafd1d35ac970d68050842c5d
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2021-09-12 12:17:09 +0200

    make theme overridable

--- src/app/style.ts
+++ src/app/style.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
+import { Theme, useTheme } from './hooks/theme.js';
 import { add, dimension, subtract } from './style/dimensions.js';
 import { color, useThemedValue } from './style/colors.js';
 import { cssEnv, cssVar, useMediaQuery } from './style/main.js';
 import { multiline } from './util/string.js';
 import { useBreakpointValue } from './style/breakpoint.js';
-import { useTheme } from './hooks/theme.js';
 
 export const strings = {
   get colorScheme() {
@@ -89,96 +89,141 @@ const staticColors = {
 };
 
 const themedColors = {
-  backgroundPrimary: (a?: number) => () =>
-    useThemedValue({
-      dark: staticColors.black,
-      dim: staticColors.black,
-      light: staticColors.white,
-    })(a),
-  backgroundSecondary: (a?: number) => () =>
-    useThemedValue({
-      dark: staticColors.blackShaded,
-      dim: staticColors.blackShaded,
-      light: staticColors.whiteShaded,
-    })(a),
-  backgroundTertiary: (a?: number) => () =>
-    useThemedValue({
-      dark: staticColors.blackShaded,
-      dim: staticColors.blackShaded,
-      light: staticColors.greyLight,
-    })(a),
-  fontPrimary: (a?: number) => () =>
-    useThemedValue({
-      dark: staticColors.white,
-      dim: staticColors.white,
-      light: staticColors.black,
-    })(a),
-  fontSecondary: (a?: number) => () =>
-    useThemedValue({
-      dark: staticColors.greyLow,
-      dim: staticColors.greyLow,
-      light: staticColors.greyLow,
-    })(a),
-  fontTertiary: (a?: number) => () =>
-    useThemedValue({
-      dark: staticColors.greyGlow,
-      dim: staticColors.greyGlow,
-      light: staticColors.greyMid,
-    })(a),
-  selection: (a?: number) => () =>
-    useThemedValue({
-      dark: staticColors.orange,
-      dim: staticColors.orange,
-      light: staticColors.blue,
-    })(a),
-  statusBarBackground: (a?: number) => () =>
-    useThemedValue({
-      dark: staticColors.blackShaded,
-      dim: staticColors.blackShaded,
-      light: staticColors.black,
-    })(a),
-  surface1: (a?: number) => () =>
-    useThemedValue({
-      dark: color(brand.hue, 10, 10),
-      dim: color(brand.hue, 10, 20),
-      light: color(brand.hue, 25, 90),
-    })(a),
-  surface2: (a?: number) => () =>
-    useThemedValue({
-      dark: color(brand.hue, 10, 15),
-      dim: color(brand.hue, 10, 25),
-      light: color(brand.hue, 20, 99),
-    })(a),
-  surface3: (a?: number) => () =>
-    useThemedValue({
-      dark: color(brand.hue, 5, 20),
-      dim: color(brand.hue, 5, 30),
-      light: color(brand.hue, 20, 92),
-    })(a),
-  surface4: (a?: number) => () =>
-    useThemedValue({
-      dark: color(brand.hue, 5, 25),
-      dim: color(brand.hue, 5, 35),
-      light: color(brand.hue, 20, 85),
-    })(a),
-  surfaceShadow: (a?: number) => () =>
-    useThemedValue({
-      dark: color(brand.hue, 50, 3),
-      dim: color(brand.hue, 30, 13),
-      light: color(brand.hue, 10, 20),
-    })(a),
-  text1: (a?: number) => () =>
-    useThemedValue({
-      dark: color(brand.hue, 15, 85),
-      dim: color(brand.hue, 15, 75),
-      light: color(brand.hue, brand.saturation, 10),
-    })(a),
-  text2: (a?: number) => () =>
-    useThemedValue({
-      dark: color(brand.hue, 5, 65),
-      dim: color(brand.hue, 10, 61),
-      light: color(brand.hue, 30, 10),
-    })(a),
+  backgroundPrimary: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: staticColors.black,
+        dim: staticColors.black,
+        light: staticColors.white,
+      },
+      themeOverride
+    )(a),
+  backgroundSecondary: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: staticColors.blackShaded,
+        dim: staticColors.blackShaded,
+        light: staticColors.whiteShaded,
+      },
+      themeOverride
+    )(a),
+  backgroundTertiary: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: staticColors.blackShaded,
+        dim: staticColors.blackShaded,
+        light: staticColors.greyLight,
+      },
+      themeOverride
+    )(a),
+  fontPrimary: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: staticColors.white,
+        dim: staticColors.white,
+        light: staticColors.black,
+      },
+      themeOverride
+    )(a),
+  fontSecondary: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: staticColors.greyLow,
+        dim: staticColors.greyLow,
+        light: staticColors.greyLow,
+      },
+      themeOverride
+    )(a),
+  fontTertiary: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: staticColors.greyGlow,
+        dim: staticColors.greyGlow,
+        light: staticColors.greyMid,
+      },
+      themeOverride
+    )(a),
+  selection: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: staticColors.orange,
+        dim: staticColors.orange,
+        light: staticColors.blue,
+      },
+      themeOverride
+    )(a),
+  statusBarBackground: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: staticColors.blackShaded,
+        dim: staticColors.blackShaded,
+        light: staticColors.black,
+      },
+      themeOverride
+    )(a),
+  surface1: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: color(brand.hue, 10, 10),
+        dim: color(brand.hue, 10, 20),
+        light: color(brand.hue, 25, 90),
+      },
+      themeOverride
+    )(a),
+  surface2: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: color(brand.hue, 10, 15),
+        dim: color(brand.hue, 10, 25),
+        light: color(brand.hue, 20, 99),
+      },
+      themeOverride
+    )(a),
+  surface3: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: color(brand.hue, 5, 20),
+        dim: color(brand.hue, 5, 30),
+        light: color(brand.hue, 20, 92),
+      },
+      themeOverride
+    )(a),
+  surface4: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: color(brand.hue, 5, 25),
+        dim: color(brand.hue, 5, 35),
+        light: color(brand.hue, 20, 85),
+      },
+      themeOverride
+    )(a),
+  surfaceShadow: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: color(brand.hue, 50, 3),
+        dim: color(brand.hue, 30, 13),
+        light: color(brand.hue, 10, 20),
+      },
+      themeOverride
+    )(a),
+  text1: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: color(brand.hue, 15, 85),
+        dim: color(brand.hue, 15, 75),
+        light: color(brand.hue, brand.saturation, 10),
+      },
+      themeOverride
+    )(a),
+  text2: (a?: number, themeOverride?: Theme) => () =>
+    useThemedValue(
+      {
+        dark: color(brand.hue, 5, 65),
+        dim: color(brand.hue, 10, 61),
+        light: color(brand.hue, 30, 10),
+      },
+      themeOverride
+    )(a),
 };
 
 export const colors = {
@@ -186,14 +231,17 @@ export const colors = {
   ...themedColors,
 };
 
-export const shadow = () => {
+export const shadow = (themeOverride?: Theme) => {
   return () => {
     const shadowSurface = colors.surfaceShadow;
-    const shadowStength = useThemedValue({
-      dark: 80,
-      dim: 20,
-      light: 2,
-    });
+    const shadowStength = useThemedValue(
+      {
+        dark: 80,
+        dim: 20,
+        light: 2,
+      },
+      themeOverride
+    );
 
     const result = multiline`
       0 2.8px 2.2px ${shadowSurface(shadowStength + 3)()},
--- src/app/style/colors.ts
+++ src/app/style/colors.ts
@@ -2,10 +2,13 @@ import { Theme, useTheme } from '../hooks/theme.js';
 
 export type Color = (a?: number) => string;
 
-export function useThemedValue<T>(themedColors: Record<Theme, T>): T {
+export function useThemedValue<T>(
+  themedColors: Record<Theme, T>,
+  themeOverride?: Theme
+): T {
   const { theme } = useTheme();
 
-  return themedColors[theme];
+  return themedColors[themeOverride || theme];
 }
 
 export const color = (h: number, s: number, l: number): Color => {
