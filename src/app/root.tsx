625c10f94d82aee9dc86f7a7a82349612d9a1f30
Lennart Pelz <mail@mrpelz.de>
2026-08-07 23:34:38 +0200

    chore: upgrade deps

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -13,53 +13,18 @@ import './state/visibility.js';
 import { setup } from 'goober';
 import { createGlobalStyles as createGlobalStyle } from 'goober/global';
 import { prefix } from 'goober/prefixer';
-import { KonstaProvider } from 'konsta/react';
 import { FunctionComponent, h, render as preactRender } from 'preact';
 
-import { dimensions, strings } from './style.js';
 import { App } from './views/app.js';
 
 const GlobalStyles = createGlobalStyle`
-  *:not(input, select, button) {
-    box-sizing: border-box;
-    scrollbar-width: none;
-    -webkit-tap-highlight-color: transparent;
-    touch-action: manipulation;
-    -webkit-touch-callout: none;
-    user-select: none;
-
-    &::-webkit-scrollbar {
-      display: none;
-    }
-  }
-
-  :root,
-  body {
-    padding: 0;
-    margin: 0;
-  }
-
-  :root {
-    display: flow-root;
-
-    /* --safe-area-inset-top: 20px; */
-    color-scheme: ${strings.colorScheme};
-    font-family: ${strings.font};
-    font-size: ${dimensions.fontSize};
-    scroll-padding: ${dimensions.headerHeight} 0 0 0;
-    scroll-snap-type: block;
-  }
+  /* noop */
 ` as unknown as FunctionComponent;
 
 export const Root: FunctionComponent = () => (
   <>
     <GlobalStyles />
-    <KonstaProvider
-      dark
-      theme="ios"
-    >
-      <App />
-    </KonstaProvider>
+    <App />
   </>
 );
 
