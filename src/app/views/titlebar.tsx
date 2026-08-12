c5c029d6ff9bbbbdb7e4e2daf0888a1aca61554b
Lennart Pelz <mail@mrpelz.de>
2026-08-12 16:16:03 +0200

    chore: [WIP]

--- src/app/views/titlebar.tsx
+++ src/app/views/titlebar.tsx
@@ -1,15 +1,10 @@
+import { Icon, Link, Navbar } from 'konsta/react';
 import {
   AnimationEventHandler,
   FunctionComponent,
   TargetedAnimationEvent,
 } from 'preact';
-import {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'preact/hooks';
+import { useCallback, useMemo } from 'preact/hooks';
 
 import {
   BackIcon,
@@ -18,11 +13,6 @@ import {
   ReturnIcon,
   WaitIcon,
 } from '../components/icons.js';
-import {
-  IconContainer as IconContainerComponent,
-  Title,
-  Titlebar as TitlebarComponent,
-} from '../components/titlebar.js';
 import { useIsWebSocketOnline } from '../hooks/use-api.js';
 import { useAwaitEvent } from '../hooks/use-await-event.js';
 import { flipMenuVisible } from '../state/menu.js';
@@ -37,40 +27,6 @@ import { capitalizedTitle$ } from '../state/title.js';
 import { dimensions } from '../style.js';
 import { useBreakpoint } from '../style/breakpoint.js';
 import { getMediaQuery } from '../style/main.js';
-
-export const IconContainer: FunctionComponent<{
-  paddingSetter: (input: number) => void;
-  right?: true;
-}> = ({ children, right, paddingSetter }) => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  const observerCallback = useCallback(() => {
-    if (!ref.current) return;
-
-    paddingSetter(ref.current.clientWidth);
-  }, [paddingSetter, ref]);
-
-  const observerRef = useRef(new MutationObserver(observerCallback));
-
-  useEffect(() => {
-    const observer = observerRef.current;
-
-    if (ref.current) {
-      observer.observe(ref.current, { childList: true, subtree: true });
-    }
-
-    return () => observer.disconnect();
-  }, [ref]);
-
-  return (
-    <IconContainerComponent
-      ref={ref}
-      right={right}
-    >
-      {children}
-    </IconContainerComponent>
-  );
-};
 
 const WaitIconView: FunctionComponent = () => {
   const { value: isWebSocketOnline } = useIsWebSocketOnline();
@@ -98,14 +54,6 @@ const WaitIconView: FunctionComponent = () => {
 };
 
 export const Titlebar: FunctionComponent = () => {
-  const [paddingLeft, setPaddingLeft] = useState(0);
-  const [paddingRight, setPaddingRight] = useState(0);
-
-  const padding = useMemo(
-    () => Math.max(paddingLeft, paddingRight),
-    [paddingLeft, paddingRight],
-  );
-
   const { value: title } = capitalizedTitle$;
 
   const isDesktop = useBreakpoint(getMediaQuery(dimensions.breakpointDesktop));
@@ -115,41 +63,72 @@ export const Titlebar: FunctionComponent = () => {
   const isMap = useMemo(() => rootPath === 'map', [rootPath]);
   const isRoot = isRoot$.value;
 
-  const leftIcon = useMemo(() => {
+  const [leftIcon, onLeftIconClick] = useMemo(() => {
     if (!isRoot) {
-      return <BackIcon onClick={goUp} />;
+      return [<BackIcon key={0} />, goUp] as const;
     }
 
-    if (isDesktop) return null;
+    if (isDesktop) return [null, null] as const;
 
-    return <MenuIcon onClick={flipMenuVisible} />;
+    return [<MenuIcon key={0} />, flipMenuVisible] as const;
   }, [isDesktop, isRoot]);
 
-  const rightIcon = useMemo(() => {
+  // eslint-disable-next-line new-cap, unicorn/no-invalid-argument-count
+  const waitIcon = WaitIconView({});
+
+  const [rightIcon, onRightIconClick] = useMemo(() => {
     if (!isDesktop && !isRoot) {
-      return <MenuIcon onClick={flipMenuVisible} />;
+      return [<MenuIcon key={0} />, flipMenuVisible] as const;
     }
 
     if (isMap) {
-      return <ReturnIcon onClick={() => goPrevious()} />;
+      return [<ReturnIcon key={0} />, () => goPrevious()] as const;
     }
 
-    return <MapIcon onClick={() => setRootPath('map')} />;
+    return [<MapIcon key={0} />, () => setRootPath('map')] as const;
   }, [isDesktop, isMap, isRoot]);
 
   return (
-    <TitlebarComponent padding={padding}>
-      {title ? <Title>{title}</Title> : null}
-      {leftIcon ? (
-        <IconContainer paddingSetter={setPaddingLeft}>{leftIcon}</IconContainer>
-      ) : null}
-      <IconContainer
-        right
-        paddingSetter={setPaddingRight}
-      >
-        <WaitIconView />
-        {rightIcon}
-      </IconContainer>
-    </TitlebarComponent>
+    <Navbar
+      large
+      title={title}
+      left={
+        leftIcon ? (
+          <Link
+            iconOnly
+            onClick={onLeftIconClick}
+          >
+            <Icon
+              className="w-5 h-5"
+              ios={leftIcon}
+              material={leftIcon}
+            />
+          </Link>
+        ) : null
+      }
+      right={
+        <>
+          {waitIcon ? (
+            <Link iconOnly>
+              <Icon
+                className="w-5 h-5"
+                ios={waitIcon}
+                material={waitIcon}
+              />
+            </Link>
+          ) : null}
+          <Link
+            iconOnly
+            onClick={onRightIconClick}
+          >
+            <Icon
+              className="w-5 h-5"
+              ios={rightIcon}
+              material={rightIcon}
+            />
+          </Link>
+        </>
+      }
+    />
   );
 };
